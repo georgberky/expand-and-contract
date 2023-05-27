83968dad801975959ef0caff2a396bc42931d15e
Georg Berky <georg.berky@posteo.de>
2023-05-27 19:07:31 +0200

    Remove verification method from business class

--- src/main/java/dev/berky/georg/simple/VisaApplicationChecks.java
+++ src/main/java/dev/berky/georg/simple/VisaApplicationChecks.java
@@ -3,17 +3,9 @@ package dev.berky.georg.simple;
 public class VisaApplicationChecks {
 
     public void checkVisaApplication(VisaApplicationId visaApplicationId) {
-        verify(visaApplicationId.raw());
         part1(visaApplicationId.raw());
         part2(visaApplicationId.raw());
         part3(visaApplicationId.raw());
-    }
-
-    private void verify(String visaApplicationId) {
-        // a lot of verification code
-        // possibly duplicated all over the place
-        // or hidden in helper functions with unguessable names
-        // because we're working on Strings
     }
 
     private void part1(String visaApplicationId) {
