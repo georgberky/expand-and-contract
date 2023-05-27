0dc35a8956263a377b42afd9b99ea491ab28520b
Georg Berky <georg.berky@posteo.de>
2023-05-27 19:06:25 +0200

    Copy verification method into record

--- src/main/java/dev/berky/georg/simple/VisaApplicationId.java
+++ src/main/java/dev/berky/georg/simple/VisaApplicationId.java
@@ -1,3 +1,14 @@
 package dev.berky.georg.simple;
 
-public record VisaApplicationId(String raw) {}
+public record VisaApplicationId(String raw) {
+    public VisaApplicationId {
+        verify(raw);
+    }
+
+    private void verify(String visaApplicationId) {
+        // a lot of verification code
+        // possibly duplicated all over the place
+        // or hidden in helper functions with unguessable names
+        // because we're working on Strings
+    }
+}
