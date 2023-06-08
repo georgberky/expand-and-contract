71487968aa92650eef4cb27b4a114b6130d33db4
Georg Berky <georg.berky@posteo.de>
2023-06-08 15:17:42 +0200

    Replace other occurrences of decreasing quality

--- src/main/java/dev/berky/georg/GildedRose.java
+++ src/main/java/dev/berky/georg/GildedRose.java
@@ -51,7 +51,7 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                                gildedItems[i].decreaseQuality();
                             }
                         }
                     } else {
