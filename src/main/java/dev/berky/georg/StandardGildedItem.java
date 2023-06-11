fe712ca7b0a703ac2f7281a6ff31db1ba23d0233
Georg Berky <georg.berky@posteo.de>
2023-06-12 00:35:47 +0200

    Push updateQuality down to StandardGildedItem

--- src/main/java/dev/berky/georg/StandardGildedItem.java
+++ src/main/java/dev/berky/georg/StandardGildedItem.java
@@ -26,4 +26,56 @@ public class StandardGildedItem implements GildedItem {
     public void decreaseSellIn() {
         this.item.sellIn--;
     }
+
+    @Override
+    public void updateQuality(Item item) {
+        if (!item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decreaseQuality();
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                increaseQuality();
+
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            increaseQuality();
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            increaseQuality();
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            decreaseSellIn();
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decreaseQuality();
+                        }
+                    }
+                } else {
+                    loseAllQuality();
+                }
+            } else {
+                if (item.quality < 50) {
+                    increaseQuality();
+                }
+            }
+        }
+    }
 }
