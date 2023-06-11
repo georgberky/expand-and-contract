25c421368e04af3753c318a330a6809fc990e1be
Georg Berky <georg.berky@posteo.de>
2023-06-12 01:20:55 +0200

    Extract BackstagePasses

--- src/main/java/dev/berky/georg/GildedItem.java
+++ src/main/java/dev/berky/georg/GildedItem.java
@@ -4,6 +4,7 @@ public interface GildedItem {
     static GildedItem from(Item item) {
         return switch(item.name) {
             case "Aged Brie" -> new AgedBrie(item);
+            case "Backstage passes to a TAFKAL80ETC concert" -> new BackstagePasses(item);
             default -> new StandardGildedItem(item);
         };
     }
