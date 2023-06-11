5fe69d24f7c71bfc1fcf37b281e4fb607b9b9705
Georg Berky <georg.berky@posteo.de>
2023-06-12 01:33:11 +0200

    Simplify updateQuality in GildedRose

--- src/main/java/dev/berky/georg/GildedRose.java
+++ src/main/java/dev/berky/georg/GildedRose.java
@@ -14,11 +14,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            var gildedItem = gildedItems[i];
-
-            gildedItem.updateQuality();
-        }
+        Arrays.stream(gildedItems)
+            .forEach(GildedItem::updateQuality);
     }
 
 }
