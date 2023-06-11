b5234e6c4e720e108d6312db7c6c8df6fd07b9c2
Georg Berky <georg.berky@posteo.de>
2023-06-12 01:24:23 +0200

    Simplify updateQuality in Sulfuras - to nothing

--- src/main/java/dev/berky/georg/Sulfuras.java
+++ src/main/java/dev/berky/georg/Sulfuras.java
@@ -30,53 +30,6 @@ public class Sulfuras implements GildedItem {
 
     @Override
     public void updateQuality() {
-        if (!name.equals("Aged Brie")
-                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    decreaseQuality();
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                increaseQuality();
-
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            increaseQuality();
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            increaseQuality();
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            decreaseSellIn();
-        }
-
-        if (item.sellIn < 0) {
-            if (!name.equals("Aged Brie")) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            decreaseQuality();
-                        }
-                    }
-                } else {
-                    loseAllQuality();
-                }
-            } else {
-                if (item.quality < 50) {
-                    increaseQuality();
-                }
-            }
-        }
+        //nothing to do
     }
 }
