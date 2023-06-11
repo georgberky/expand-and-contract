15d28a57a8fffc2fe8d5ff6a5b454da7c17fee33
Georg Berky <georg.berky@posteo.de>
2023-06-12 01:05:00 +0200

    Add AgedBrie as copy

--- src/main/java/dev/berky/georg/GildedItem.java
+++ src/main/java/dev/berky/georg/GildedItem.java
@@ -3,6 +3,7 @@ package dev.berky.georg;
 public interface GildedItem {
     static GildedItem from(Item item) {
         return switch(item.name) {
+            case "Aged Brie" -> new AgedBrie(item);
             default -> new StandardGildedItem(item);
         };
     }
