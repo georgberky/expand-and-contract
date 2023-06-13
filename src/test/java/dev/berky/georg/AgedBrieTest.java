a6aeb4d7b9fe9ed57a038f45f4f58bb9ef7bd9ca
Georg Berky <georg.berky@posteo.de>
2023-06-13 23:45:09 +0200

    Start migration of AgedBrie

--- src/test/java/dev/berky/georg/AgedBrieTest.java
+++ src/test/java/dev/berky/georg/AgedBrieTest.java
@@ -6,12 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static dev.berky.georg.GildedRoseFixtures.anySellInDate;
 import static dev.berky.georg.GildedRoseFixtures.maxQuality;
 import static dev.berky.georg.GildedRoseFixtures.notPastSellInDate;
 import static dev.berky.georg.GildedRoseFixtures.pastSellInDate;
+
+import static dev.berky.georg.GildedRoseFixtures.givenItem;
 import static dev.berky.georg.GildedRoseFixtures.whenOneDayPasses;
-import static org.assertj.core.api.Assertions.assertThat;
+import static dev.berky.georg.GildedRoseFixtures.assertThatItemHasQuality;
 
 class AgedBrieTest {
 
@@ -19,31 +23,34 @@ class AgedBrieTest {
     @ValueSource(ints = { 10, 11, 12 })
     @DisplayName("Aged Brie: increases quality")
     void whenDayPasses_itemAgedBrie_shouldIncreaseQualityByOne(int initialQuality) {
-        Item item = new Item("Aged Brie", notPastSellInDate(), initialQuality);
+        var item = givenItem("Aged Brie", notPastSellInDate(), initialQuality);
 
         whenOneDayPasses(item);
 
         assertThat(item.quality).isEqualTo(initialQuality + 1);
+        assertThatItemHasQuality(item, initialQuality + 1);
     }
 
     @Test
     @DisplayName("Aged Brie: does not increase quality above maximum")
     void whenDayPasses_itemAgedBrieWithQualityFifty_shouldNotIncreaseQuality() {
-        Item item = new Item("Aged Brie", anySellInDate(), maxQuality());
+        var item = givenItem("Aged Brie", anySellInDate(), maxQuality());
 
         whenOneDayPasses(item);
 
         assertThat(item.quality).isEqualTo(maxQuality());
+        assertThatItemHasQuality(item, maxQuality());
     }
 
     @ParameterizedTest(name = "initial quality: {0}")
     @ValueSource(ints = { 46, 47, 48 })
     @DisplayName("Aged Brie: increases quality past sell-in date by two")
     void afterSellInDate_AgedBrie_increasesInQuality(int initialQuality) {
-        Item item = new Item("Aged Brie", pastSellInDate(), initialQuality);
+        var item = givenItem("Aged Brie", pastSellInDate(), initialQuality);
 
         whenOneDayPasses(item);
 
         assertThat(item.quality).isEqualTo(initialQuality + 2);
+        assertThatItemHasQuality(item, initialQuality + 2);
     }
 }
