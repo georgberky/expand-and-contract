08a4b13c8491fbb2c2cfdd33777be74e29762a0f
Georg Berky <georg.berky@posteo.de>
2023-06-13 23:53:18 +0200

    Remove old assertions

--- src/test/java/dev/berky/georg/SulfurasTest.java
+++ src/test/java/dev/berky/georg/SulfurasTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import static dev.berky.georg.GildedRoseFixtures.notPastSellInDate;
 
 import static dev.berky.georg.GildedRoseFixtures.givenItem;
@@ -23,7 +21,6 @@ class SulfurasTest {
 
         whenOneDayPasses(item);
 
-        assertThat(item.quality).isEqualTo(initialQuality);
         assertThatItemHasQuality(item, initialQuality);
     }
 }
