f8671d42fde28d1f5dbb37595cbead41a78eee1a
Georg Berky <georg.berky@posteo.de>
2023-06-13 23:56:40 +0200

    Contract: remove old fixtures

--- src/test/java/dev/berky/georg/GildedRoseFixtures.java
+++ src/test/java/dev/berky/georg/GildedRoseFixtures.java
@@ -1,11 +1,6 @@
 package dev.berky.georg;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 final class GildedRoseFixtures {
-    public static Item givenItem(String name, int sellIn, int quality) {
-        return new Item(name, sellIn, quality);
-    }
 
     public static int maxQuality() {
         return 50;
@@ -29,23 +24,5 @@ final class GildedRoseFixtures {
 
     public static int pastSellInDate() {
         return -1;
-    }
-
-    public static void whenOneDayPasses(Item item) {
-        GildedRose app = createApp(item);
-        app.updateQuality();
-    }
-
-    public static GildedRose createApp(Item item) {
-        Item[] items = new Item[]{item};
-        return new GildedRose(items);
-    }
-
-    public static void assertThatItemHasQuality(Item item, int expected) {
-        assertThat(item.quality).isEqualTo(expected);
-    }
-
-    public static void assertThatItemHasSellIn(Item item, int expected) {
-        assertThat(item.sellIn).isEqualTo(expected);
     }
 }
