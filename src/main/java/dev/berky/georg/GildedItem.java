64bfc6740895f57d44d5cd216d85ee9a51ff7d00
Georg Berky <georg.berky@posteo.de>
2023-06-15 23:55:03 +0200

    Remove old from()

--- src/main/java/dev/berky/georg/GildedItem.java
+++ src/main/java/dev/berky/georg/GildedItem.java
@@ -1,17 +1,15 @@
 package dev.berky.georg;
 
 public interface GildedItem {
-    static GildedItem from(Item item) {
+
+    static GildedItem from(String name, int sellIn, int quality) {
+        Item item = new Item(name, sellIn, quality);
         return switch (item.name) {
             case "Aged Brie" -> new AgedBrie(item);
             case "Backstage passes to a TAFKAL80ETC concert" -> new BackstagePasses(item);
             case "Sulfuras, Hand of Ragnaros" -> new Sulfuras(item);
             default -> new RegularItem(item);
         };
-    }
-
-    static GildedItem from(String name, int sellIn, int quality) {
-        return GildedItem.from(new Item(name, sellIn, quality));
     }
 
     String name();
