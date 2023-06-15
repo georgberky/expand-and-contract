f065b1875674eceab0f18bed83ac62f727d760a1
Georg Berky <georg.berky@posteo.de>
2023-06-16 00:13:07 +0200

    Remove Item from from()

--- src/main/java/dev/berky/georg/GildedItem.java
+++ src/main/java/dev/berky/georg/GildedItem.java
@@ -3,12 +3,11 @@ package dev.berky.georg;
 public interface GildedItem {
 
     static GildedItem from(String name, int sellIn, int quality) {
-        Item item = new Item(name, sellIn, quality);
-        return switch (item.name) {
-            case "Aged Brie" -> new AgedBrie(item.sellIn, item.quality);
-            case "Backstage passes to a TAFKAL80ETC concert" -> new BackstagePasses(item.sellIn, item.quality);
-            case "Sulfuras, Hand of Ragnaros" -> new Sulfuras(item.sellIn, item.quality);
-            default -> new RegularItem(item.name, item.sellIn, item.quality);
+        return switch (name) {
+            case "Aged Brie" -> new AgedBrie(sellIn, quality);
+            case "Backstage passes to a TAFKAL80ETC concert" -> new BackstagePasses(sellIn, quality);
+            case "Sulfuras, Hand of Ragnaros" -> new Sulfuras(sellIn, quality);
+            default -> new RegularItem(name, sellIn, quality);
         };
     }
 
